923d16ec1a733f31940a30950d8bf413f9e835c9
Alibek2002 <thealibekaltynbekuulu@gmail.com>
2022-05-14 01:34:10 +0600

    first commit

---
+++ components/Banner.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { Movie } from '../typing'
+import { baseUrl } from '../constants/movie'
+import PlayButton from '../assets/icons/play.svg'
+import InfoButton from '../assets/icons/information-button.svg'
+
+interface Props {
+  netflixOriginals?: Movie[]
+}
+
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | undefined>(undefined)
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
+
+  return (
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-full">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
+      <h1 className="text-2xl font-bold text-shadow-lg md:text-4xl lg:text-7xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs text-shadow-md text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+
+      <div className="flex space-x-3">
+        <button className="bannerButton bg-white text-black">
+          <PlayButton className="h-3 w-3 md:h-6  md:w-6" /> Play
+        </button>
+        <button className="bannerButton bg-[gray]/70">
+          <InfoButton className="h-3 w-3 md:h-6 md:w-6" />
+          More Info
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ components/Header.tsx
@@ -0,0 +1,61 @@
+import NetflixLogo from '../assets/images/logo.svg'
+import SearchButton from '../assets/icons/search.svg'
+import BellButton from '../assets/icons/bell.svg'
+import AccountButton from '../assets/icons/user.svg'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import React from 'react'
+function Header() {
+  const [isScroll, setIsScroll] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScroll(true)
+      } else {
+        setIsScroll(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <header className={`${isScroll && 'bg-[#141414]'}`}>
+      <div className="flex items-center space-x-2 py-[5px] md:space-x-4">
+        <Link href={'/'}>
+          <a>
+            <NetflixLogo className="w-[100px] cursor-pointer" />
+          </a>
+        </Link>
+        <ul className="hidden  items-center space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Tv Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My Lists</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchButton className="h-[20px] w-[20px] cursor-pointer" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellButton className="h-[20px] w-[20px] cursor-pointer" />
+        <Link href={'/account'}>
+          <a>
+            <img
+              src="http://zoeice.com/assets/img/uploads/profile.png"
+              alt="Account"
+              className="h-[30px] w-[30px] rounded-sm"
+            />
+          </a>
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
